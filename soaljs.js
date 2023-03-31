0d525a9a5690880d5656c11462da05b23dd43acf
Hidayatul Nur Hanifah <120232865+hidayatulnh@users.noreply.github.com>
2023-03-31 10:29:34 +0700

    Add files via upload

---
+++ soaljs.js
@@ -0,0 +1,19 @@
+//LOOPING No 1
+//let n = 100;
+// for (let i = 1 ; i <= n; i++) {
+//     console.log(i);
+//   }
+
+// Lopping no 2
+// for(counter = 0; counter < 20; counter+=2){
+//     console.log("Perulangan ke-"+counter);
+// }
+
+//Lopping no 3
+for(counter = 0 ;counter <= 20; counter++){
+  if (counter % 2 == 0){
+    console.log(counter +' Bilangan Genap');
+  } else{
+    console.log(counter +' Bilangan Ganjil');;
+}
+}
